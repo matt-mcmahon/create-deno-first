b904d6d1fc57be2b587941ed1e6319a00feafeb0
Matt McMahon <contact@matt-mcmahon.com>
2020-11-18 13:10:40 -0500

    WIP

--- source/configure/cache.ts
+++ source/configure/cache.ts
@@ -1,10 +1,8 @@
-import { assertEquals as assert } from "../../remote/asserts.ts";
-import { askYesNo, IO, question } from "../../remote/fluentty.ts";
+import { askYesNo, isYes, question } from "../../remote/fluentty.ts";
 import { ifElse } from "../../remote/functional.ts";
-import { ConfigModule } from "../configure.ts";
 
-export const configDenoDir: ConfigModule = (defaultTo: string): () => Promise<[string, string][]> {
-  return async () =>
+export const configDenoDir = (defaultTo: string) =>
+  () =>
     question("Local Deno cache directory:")
       .defaultTo(defaultTo)
       .andSuggest()
@@ -12,7 +10,6 @@ export const configDenoDir: ConfigModule = (defaultTo: string): () => Promise<[s
       .then(
         (denoDir) => [["USE_CACHE", "--cached-only"], ["DENO_DIR", denoDir]],
       );
-}
 
 const disableCache = () => [["USE_CACHE", ""]];
 
@@ -25,43 +22,13 @@ const configDenoCache = (defaultTo: string) =>
   askYesNo("Enable local Deno cache?")
     .defaultTo(defaultTo).justAccept();
 
-export const configCache = function* (defaultTo: string) {
-  const q1 = [
-    configDenoCache(Deno.env.get("USE_CACHE") ? "yes" : "no"),
-  ];
-  const a1 = yield q1;
-
-  const afterIO = [
+export const configCache = (defaultTo: string) => {
+  const useCache = configDenoCache(Deno.env.get("USE_CACHE") ? "yes" : "no");
+  useCache.then(() =>
     ifElse(
-      (a) => a === "yes",
-      () => configDenoDir(Deno.env.get("DENO_DIR") ?? ".deno"),
+      isYes,
+      configDenoDir(Deno.env.get("DENO_DIR") ?? ".deno"),
       disableCache,
-    ),
-  ];
-
-  return q1;
+    )
+  );
 };
-
-Deno.test("configCache", async () => {
-  const doIO = configCache("yes");
-
-  const q1 = doIO.next().value;
-  const a1 = await IO(...q1);
-
-  {
-    const actual = a1;
-    const expected = ["yes"];
-    assert(actual, expected);
-  }
-
-  const q2 = doIO.next().value;
-  const a2 = await IO(...q1);
-
-  {
-    const actual = a2;
-    const expected = [""];
-    assert(actual, expected);
-  }
-
-  console.dir(a1);
-});
